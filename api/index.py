bbd575c7176fbb6eb3770fc7936b20237f8c31a3
libkluid <kluid.me@yahoo.com>
2024-05-07 01:26:33 +0900

    chore: vercel

--- api/index.py
+++ api/index.py
@@ -1,12 +1,6 @@
 from fastapi import FastAPI
 from api.routes import debug
 
-app = FastAPI(
-    title="FastAPI Demo",
-    description="This is a simple FastAPI demo.",
-    version="0.1",
-    docs_url="/docs",
-    redoc_url=None,
-)
+app = FastAPI()
 
 app.include_router(debug.router)
