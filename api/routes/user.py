cfd378593174567f65f5003dff5b493f8555fef4
libkluid <kluid.me@yahoo.com>
2024-05-14 23:57:23 +0900

    feat: get user profile

--- api/routes/user.py
+++ api/routes/user.py
@@ -7,10 +7,24 @@ from api.auth import verify_user
 
 router = APIRouter(prefix="/user", tags=["user"])
 
-@router.patch(
-    path="/",
+@router.get(
+    path="",
+    summary="Get user profile",
+    description="Get user profile",
 )
 async def get_user(
+    user: Annotated[models.User, Depends(verify_user)],
+    user_repository: Annotated[UserRepository, Depends(UserRepository)]
+) -> models.UserProfile:
+    profile = await user_repository.get_profile(user)
+    return profile
+
+@router.patch(
+    path="",
+    summary="Update user profile",
+    description="Update user profile",
+)
+async def update_user(
     profile: models.UserProfile,
     user: Annotated[models.User, Depends(verify_user)],
     user_repository: Annotated[UserRepository, Depends(UserRepository)]
